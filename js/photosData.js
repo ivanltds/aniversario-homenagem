e70ea5fc4ede982b524ae8f5f42dd8b3ccaf2871
Ivan Souza <ivanlucio@bemol.com.br>
2026-05-25 00:20:53 -0300

    feat(fotos): adiciona novas fotos e atualiza lista

--- js/photosData.js
+++ js/photosData.js
@@ -1,26 +1,41 @@
 // Arquivo gerado automaticamente pelo build
 export const photosData = {
   "sozinha": [
-    "fotos/sozinha/20250706_164449.jpg"
+    "fotos/sozinha/20250706_162514.jpg",
+    "fotos/sozinha/20250706_164449.jpg",
+    "fotos/sozinha/20250926_214227.jpg",
+    "fotos/sozinha/IMG_20251013_211653_183.jpg",
+    "fotos/sozinha/Screenshot_2023-07-11-23-12-50-114_com.whatsapp.jpg"
   ],
   "comigo-ivan": [
     "fotos/comigo-ivan/20250209_183859.jpg",
+    "fotos/comigo-ivan/20250526_044742.jpg",
+    "fotos/comigo-ivan/20250926_213708.jpg",
     "fotos/comigo-ivan/20250926_220939.jpg",
     "fotos/comigo-ivan/20251101_233301.jpg",
     "fotos/comigo-ivan/20251222_193309.jpg",
-    "fotos/comigo-ivan/20251224_234002.jpg"
+    "fotos/comigo-ivan/20251224_234002.jpg",
+    "fotos/comigo-ivan/20260222_101306.jpg",
+    "fotos/comigo-ivan/20260503_123139.jpg",
+    "fotos/comigo-ivan/20260509_194534.jpg"
   ],
   "zuadas": [
     "fotos/zuadas/20250401_175126.jpg",
     "fotos/zuadas/20250406_081526.jpg",
     "fotos/zuadas/20250411_185135.jpg",
     "fotos/zuadas/20250414_192927.jpg",
+    "fotos/zuadas/20250830_224558.jpg",
+    "fotos/zuadas/20260222_124236.jpg",
+    "fotos/zuadas/20260507_105400.jpg",
     "fotos/zuadas/IMG-20230106-WA0002.jpg",
+    "fotos/zuadas/IMG-20230106-WA0003.jpg",
+    "fotos/zuadas/IMG_20251018_230107_203.jpg",
     "fotos/zuadas/Screenshot_2023-03-07-23-50-38-327_com.google.android.apps.photos.jpg"
   ],
   "iuri": [
     "fotos/iuri/20250713_154623.jpg",
     "fotos/iuri/20250804_210955.jpg",
+    "fotos/iuri/20250823_175341.jpg",
     "fotos/iuri/20251121_163546.jpg",
     "fotos/iuri/20251220_232859 - Copia.jpg",
     "fotos/iuri/IMG_20250801_132348_435.jpg",
